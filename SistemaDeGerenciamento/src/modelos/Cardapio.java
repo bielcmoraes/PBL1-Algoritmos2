c5c0f98e08044b413c7af32e9e950a834443a006
pwdlu <fernandocintra2871@gmail.com>
2022-03-19 22:00:29 -0300

    Legal Fera bem Loko

--- SistemaDeGerenciamento/src/modelos/Cardapio.java
+++ SistemaDeGerenciamento/src/modelos/Cardapio.java
@@ -6,7 +6,6 @@ public class Cardapio {
 	
 	private int id;
 	private ArrayList<Prato> pratos;
-	
 	public Cardapio(ArrayList<Prato> pratos) {
 		this.pratos = pratos;
 	}
@@ -17,7 +16,7 @@ public class Cardapio {
 		ArrayList<Prato> selecionados = new ArrayList();
 		
 		//Fazer um código que recebe um array com nome dos pratos, procura no ArrayList
-		//de pratos e adiciona em outro ArrayList e retorna esse novo ArrayList; Edit: Luis Fernando
+		//de pratos e adiciona em outro ArrayList e retorna esse novo ArrayList; Legal fera bem loko
 		
 		return selecionados;
 	}
