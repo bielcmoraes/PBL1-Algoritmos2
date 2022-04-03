77e3758e0d8446ea471b8ca27c27f8f1f929c6ae
pwdlu <fernandocintra2871@gmail.com>
2022-04-03 11:20:47 -0300

    Criação do ProdutosView

--- SistemaDeGerenciamento/src/view/ProdutosView.java
+++ SistemaDeGerenciamento/src/view/ProdutosView.java
@@ -1,24 +1,63 @@
 package view;
 
-import controller.ProdutoCopyable;
+import java.util.Scanner;
 
-public class ProdutosView implements ProdutoCopyable {
+public class ProdutosView {
 
-	@Override
-	public void cadastrarProduto() {
-		// TODO Auto-generated method stub
+	public static String[] cadastrarProduto() {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite o nome do produto");
+		String nome = input.nextLine();
+		
+		System.out.println("Digite o preço do produto");
+		String preco = input.nextLine();
+		
+		System.out.println("Digite a validade do produto");
+		String validade = input.nextLine();
+		
+		String [] info = new String[3];
+		
+		info[0] = nome;
+		info[1] = preco;
+		info[2] = validade;
+		
+		return info;
+	}
+
+	public static String buscaProduto() {
+		
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite o codigo do produto que deseja buscar");
+		String codigo = input.nextLine();
+		return codigo;
+	}
+	
+	public static String[] editarProduto() {
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Digite um novo nome de produto");
+		String nome = input.nextLine();
+		
+		System.out.println("Digite um novo preco de produto");
+		String preco = input.nextLine();
+		
+		System.out.println("Digite uma nova vailidade de produto");
+		String validade = input.nextLine();
+		
+		String [] info = new String[3];
+		
+		info[0] = nome;
+		info[1] = preco;
+		info[2] = validade;
+		
+		return info;
 		
 	}
 
-	@Override
-	public void editarProduto() {
-		// TODO Auto-generated method stub
+	public String excluirProduto() {
 		
-	}
-
-	@Override
-	public void excluirProduto() {
-		// TODO Auto-generated method stub
+		return buscaProduto();
 		
 	}
 
