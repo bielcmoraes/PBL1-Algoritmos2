88539057a239372f5b4cd2b92f321eb079392101
bielcmoraes <gcmorais66@gmail.com>
2022-04-09 22:51:14 -0300

    Exclui a classe desnecessaria "Cardapio" e documentei a classe Entidade

--- SistemaDeGerenciamento/src/model/Entidade.java
+++ SistemaDeGerenciamento/src/model/Entidade.java
@@ -3,12 +3,18 @@ package model;
 import java.util.ArrayList;
 import java.util.Random;
 
+/**Classe abstrata que contém um atributo id, além dos metódos necessarios para que o id seja único e utilizado nas classes necessarias.
+ * 
+ * @author Gabriel Moraes e Luis Fernando Cintra
+ *
+ */
 public abstract class Entidade {
 	
 	// Attributes
 	private String id;
 	
-	// Constructor
+	/**O contrutor recebe como parâmetro a lista de id's e ,utilizando a biblioteca Random, gera um id aleatório entre 0 e 10000 que não está na lista de id's.
+	 * @param listaIds*/
 	public Entidade (ArrayList<String> listaIds) {
 		Random aleatorio = new Random();
 		do {
@@ -17,12 +23,16 @@ public abstract class Entidade {
 		
 	}
 	
-	//O construtor vazio é necessário para setar o id do primeiro usuario na classe Usuario
+	/**O construtor vazio é necessário para atribuir um id ao usuario que for instanciado com construtor vazio na classe Usuario.
+	 */
 	public Entidade () {
 		this.id = "99999";
 	}
 	
-	// Methods
+	/**Metódo para retorno do id
+	 * 
+	 * @return String - Id
+	 */
 	public String getId() {
 		return id;
 	}
